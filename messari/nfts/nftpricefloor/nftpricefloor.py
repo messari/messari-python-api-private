afa54402aec116e3754b73b94c8cda3d56ead241
KremerMichael <kremeremichael@gmail.com>
2022-06-12 16:36:00 -0700

    quick fix for nftprice floor api url

--- messari/nfts/nftpricefloor/nftpricefloor.py
+++ messari/nfts/nftpricefloor/nftpricefloor.py
@@ -6,10 +6,14 @@ import pandas as pd
 from string import Template
 from typing import Union, List
 
-NFTS_URL = 'https://api.nftpricefloor.com/nfts'
-COLLECTION_URL = Template('https://api.nftpricefloor.com/nft/$collection/chart/pricefloor')
+NFTS_URL = 'https://api-bff.nftpricefloor.com/nfts'
+COLLECTION_URL = Template('https://api-bff.nftpricefloor.com/nft/$collection/chart/pricefloor')
 COLLECTION_PARAMS = {'interval':'all'}
 
+HEADERS = {
+        'Content-Type': 'application/json',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+        }
 
 class NFTPriceFloor(DataLoader):
     """This class is a wrapper around the NFTPriceFloor API
@@ -25,7 +29,7 @@ class NFTPriceFloor(DataLoader):
             DataFrame
                 DataFrame with info on all nfts
         """
-        response = self.get_response(NFTS_URL)
+        response = self.get_response(NFTS_URL,headers=HEADERS)
         return pd.DataFrame(response)
 
     def get_floor(self, collection_in: Union[str, List]) -> pd.DataFrame:
@@ -45,7 +49,7 @@ class NFTPriceFloor(DataLoader):
         df_list = []
         for collection in collections:
             endpoint_url = COLLECTION_URL.substitute(collection=collection)
-            response = self.get_response(endpoint_url, params=COLLECTION_PARAMS)
+            response = self.get_response(endpoint_url, params=COLLECTION_PARAMS, headers=HEADERS)
             tmp_df = pd.DataFrame(response)
             tmp_df.set_index('dates', inplace=True)
             df_list.append(tmp_df)
